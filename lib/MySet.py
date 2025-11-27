bb634ff3c01fb46e4d43c49fe524051fcfc84994
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2025-11-27 21:50:08 +0300

    lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,17 @@
-class MySet:
 
-    pass
+class MySet:
+    def __init__(self, iterable=None):
+        self.dictionary = {}
+        if iterable:
+            for item in iterable:
+                self.dictionary[item] = True
+
+    def add(self, item):
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        return item in self.dictionary
